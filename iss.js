05de439f7b5caf490d6ac0f9ef3c66b04b8995ec
PaolaP <paolaperez8@gmail.com>
2021-11-21 15:14:35 -0500

    index includes error checking int he request callback

--- iss.js
+++ iss.js
@@ -9,20 +9,25 @@
  * @format
  */
 const request = require("request");
-const fetchMyIP = function(callback) {
-  // use request to fetch IP address from JSON API
-  request(`https://api.ipify.org/?format=json`, (error, response, body) => {
-    if (error) {
-      callback(error, response.statusCode);
-      return;
-    }
-    const data = JSON.parse(body);
-    if (data) {
-      callback(null, data.ip);
-    } else {
-      callback(null, `ip not found`);
-    }
-  });
+const fetchMyIP = function (callback) {
+	// use request to fetch IP address from JSON API
+	request(`https://api.ipify.org/?format=json`, (error, response, body) => {
+		if (error) {
+			callback(error, null);
+			return;
+		}
+		if (response.statusCode !== 200) {
+			const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+			callback(Error(msg), null);
+			return;
+		}
+		const data = JSON.parse(body);
+		if (data) {
+			callback(null, data.ip);
+		} else {
+			callback(null, `ip not found`);
+		}
+	});
 };
 
 module.exports = { fetchMyIP };
